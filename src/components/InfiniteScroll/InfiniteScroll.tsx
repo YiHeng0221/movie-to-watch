ca79b941fa3f0f92ef9b560afb60acd554362d78
YiHeng <haha950221@gmail.com>
2024-11-29 15:59:36 +0800

    remove_grid-cols-1

--- src/components/InfiniteScroll/InfiniteScroll.tsx
+++ src/components/InfiniteScroll/InfiniteScroll.tsx
@@ -29,7 +29,7 @@ function InfiniteScroll({ type }: { type: string }) {
 
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {movies &&
           movies.map((result: Movie, index: number) => (
             <MovieCard
