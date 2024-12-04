2522faf7fb64be2a211eebc0a0569d3ac63f99cb
Allen Wu <65911123+YiHeng0221@users.noreply.github.com>
2024-12-05 02:50:22 +0800

    repeat (#2)

--- src/components/Carousel/LotteryWheel.tsx
+++ src/components/Carousel/LotteryWheel.tsx
@@ -1,6 +1,5 @@
 const LotteryWheel = ({
   spinWheel,
-  disabled,
 }: {
   spinWheel: () => void;
   disabled: boolean;
@@ -9,13 +8,11 @@ const LotteryWheel = ({
     <div className="flex flex-col items-center justify-center gap-4">
       <div className="flex flex-col items-center justify-center gap-2">
         <h1 className="text-xl font-bold text-center px-8">
-          Not sure what to watch? If you have more than ten items in your
-          favorites, let us help you pick one from it!
+          Not sure what to watch? let us help you pick one from it!
         </h1>
         <button
-          className="bg-red text-white px-4 py-2 rounded-md font-bold hover:bg-red-hover transition-all duration-300 disabled:bg-gray-400 disabled:cursor-not-allowed"
+          className="bg-red text-white px-4 py-2 rounded-md font-bold hover:bg-red-hover transition-all duration-300"
           onClick={spinWheel}
-          disabled={disabled}
         >
           SPIN
         </button>
