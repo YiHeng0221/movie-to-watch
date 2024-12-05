6e5b8a5aa53e41b535c9e7ca95bf8fa3a1590832
YiHeng <haha950221@gmail.com>
2024-12-06 06:07:01 +0800

    card_skeleton

--- src/components/Cards/Credit.tsx
+++ src/components/Cards/Credit.tsx
@@ -7,19 +7,25 @@ const CreditCard = ({
   character,
 }: {
   name: string;
-  image: string;
+  image: string | null;
   character: string;
 }) => {
   const url = getImageUrl(image, 500);
+  console.log(image)
   return (
     <div className="max-w-sm bg-black rounded-lg shadow w-32 relative group">
-      <Image
+      {image ? (<Image
         className="rounded-t-lg min-h-48"
         src={url}
         alt={name}
         width={500}
-        height={500}
-      />
+          height={500}
+        />
+      ) : (
+        <div className="rounded-t-lg min-h-48 bg-gray-black flex items-center justify-center">
+          <p className="text-white text-sm">No image</p>
+        </div>
+      )}
       <div className="p-2">
         <h5 className="text-md font-bold tracking-tight text-white line-clamp-1">
           {name}
